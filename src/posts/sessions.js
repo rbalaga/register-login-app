870619eae01c9aa3de00a1e596afb5fedf567f98
RAMMOHANA RAO BALAGA <ramanji.balaga@gmail.com>
2021-04-18 22:53:14 +0530

    multiple added

--- src/posts/sessions.js
+++ src/posts/sessions.js
@@ -4,6 +4,7 @@ var request = require("request");
 var router = express.Router();
 router.route("/").get(async function (req, res, next) {
   try {
+    let data = [];
     const response = request(
       {
         url: "https://dev.codementor.io/api/sessions",
@@ -13,8 +14,37 @@ router.route("/").get(async function (req, res, next) {
       },
       (error, response, body) => {
         if (!error && response.statusCode === 200) {
-          const data = JSON.parse(body);
-          res.json(data);
+          const results = JSON.parse(body);
+          data = [...data, ...results];
+          request(
+            {
+              url: "https://dev.codementor.io/api/direct-payments",
+              headers: {
+                "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+              }
+            },
+            (error, response, body) => {
+              if (!error && response.statusCode === 200) {
+                const results = JSON.parse(body);
+                data = [...data, ...results];
+                request(
+                  {
+                    url: "https://dev.codementor.io/api/freelance-jobs",
+                    headers: {
+                      "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+                    }
+                  },
+                  (error, response, body) => {
+                    if (!error && response.statusCode === 200) {
+                      const results = JSON.parse(body);
+                      data = [...data, ...results];
+                      res.json(data);
+                    }
+                  }
+                );
+              }
+            }
+          );
         }
       }
     );
