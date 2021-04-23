6600c1479640af53e158689edd6bc4e5ee5fe7a6
RAMMOHANA RAO BALAGA <ramanji.balaga@gmail.com>
2021-04-23 14:16:36 +0530

    ad asd

--- src/posts/sessions.js
+++ src/posts/sessions.js
@@ -17,72 +17,14 @@ const getLatestTime = () => {
 
 let latestTime = parseInt(getLatestTime(), 10);
 
-router.route("/time").get((req, res, next) => {
-  res.send("LATEST TIME:" + latestTime);
-});
 router
-  .route("/")
-  .get(async function (req, res, next) {
-    try {
-      let data = [];
-      const response = request(
-        {
-          url: "https://dev.codementor.io/api/sessions",
-          headers: {
-            "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
-          }
-        },
-        (error, response, body) => {
-          if (!error && response.statusCode === 200) {
-            const results = JSON.parse(body);
-
-            data = [...data, ...results.data];
-            data = data.filter((d) => d.finished_at >= latestTime);
-            request(
-              {
-                url: "https://dev.codementor.io/api/direct-payments",
-                headers: {
-                  "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
-                }
-              },
-              (error, response, body) => {
-                if (!error && response.statusCode === 200) {
-                  const results = JSON.parse(body);
-                  data = [...data, ...results.data];
-                  data = data.filter((d) => d.finished_at >= latestTime);
-                  request(
-                    {
-                      url: "https://dev.codementor.io/api/freelance-jobs",
-                      headers: {
-                        "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
-                      }
-                    },
-                    (error, response, body) => {
-                      if (!error && response.statusCode === 200) {
-                        const results = JSON.parse(body);
-                        data = [...data, ...results.data];
-                        data = data.filter((d) => d.finished_at >= latestTime);
-                        data = data.sort(
-                          (a, b) => a.finished_at > b.finished_at
-                        );
-                        res.json(data);
-                      }
-                    }
-                  );
-                }
-              }
-            );
-          }
-        }
-      );
-    } catch (error) {
-      console.log("failed: " + error.message);
-      res.end("failed: " + error.message);
-    }
+  .route("/time")
+  .get((req, res, next) => {
+    res.send("LATEST TIME:" + latestTime);
   })
   .post((req, res, next) => {
-    latestTime = req.body.latestTime;
-    fs.writeFile(path.join(__dirname, "/latest"), latestTime, (err) => {
+    latestTime = req.body.time;
+    fs.writeFile(path.join(__dirname, "latest"), latestTime, (err) => {
       if (err) {
         console.error(err);
         return;
@@ -90,5 +32,62 @@ router
       res.end("success");
     });
   });
+
+router.route("/").get(async function (req, res, next) {
+  try {
+    let data = [];
+    const response = request(
+      {
+        url: "https://dev.codementor.io/api/sessions",
+        headers: {
+          "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+        }
+      },
+      (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const results = JSON.parse(body);
+
+          data = [...data, ...results.data];
+          data = data.filter((d) => d.finished_at >= latestTime);
+          request(
+            {
+              url: "https://dev.codementor.io/api/direct-payments",
+              headers: {
+                "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+              }
+            },
+            (error, response, body) => {
+              if (!error && response.statusCode === 200) {
+                const results = JSON.parse(body);
+                data = [...data, ...results.data];
+                data = data.filter((d) => d.finished_at >= latestTime);
+                request(
+                  {
+                    url: "https://dev.codementor.io/api/freelance-jobs",
+                    headers: {
+                      "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+                    }
+                  },
+                  (error, response, body) => {
+                    if (!error && response.statusCode === 200) {
+                      const results = JSON.parse(body);
+                      data = [...data, ...results.data];
+                      data = data.filter((d) => d.finished_at >= latestTime);
+                      data = data.sort((a, b) => a.finished_at > b.finished_at);
+                      res.json(data);
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+  } catch (error) {
+    console.log("failed: " + error.message);
+    res.end("failed: " + error.message);
+  }
+});
 
 module.exports = router;
