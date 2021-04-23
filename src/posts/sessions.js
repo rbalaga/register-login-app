f615697889beb0a2c8849ad70f28afd22087976e
RAMMOHANA RAO BALAGA <ramanji.balaga@gmail.com>
2021-04-23 13:38:25 +0530

    provided filters and sorting

--- src/posts/sessions.js
+++ src/posts/sessions.js
@@ -1,57 +1,88 @@
 var express = require("express");
 var request = require("request");
+var fs = require("fs");
 
 var router = express.Router();
-router.route("/").get(async function (req, res, next) {
-  try {
-    let data = [];
-    const response = request(
-      {
-        url: "https://dev.codementor.io/api/sessions",
-        headers: {
-          "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
-        }
-      },
-      (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const results = JSON.parse(body);
-          data = [...data, ...results.data];
-          request(
-            {
-              url: "https://dev.codementor.io/api/direct-payments",
-              headers: {
-                "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
-              }
-            },
-            (error, response, body) => {
-              if (!error && response.statusCode === 200) {
-                const results = JSON.parse(body);
-                data = [...data, ...results.data];
-                request(
-                  {
-                    url: "https://dev.codementor.io/api/freelance-jobs",
-                    headers: {
-                      "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+const getLatestTime = () => {
+  fs.readFile("./latest", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    return data;
+  });
+};
+let latestTime = parseInt(getLatestTime, 10);
+
+router
+  .route("/")
+  .get(async function (req, res, next) {
+    try {
+      let data = [];
+      const response = request(
+        {
+          url: "https://dev.codementor.io/api/sessions",
+          headers: {
+            "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+          }
+        },
+        (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const results = JSON.parse(body);
+
+            data = [...data, ...results.data];
+            data = data.filter((d) => d.finished_at >= latestTime);
+            request(
+              {
+                url: "https://dev.codementor.io/api/direct-payments",
+                headers: {
+                  "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+                }
+              },
+              (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                  const results = JSON.parse(body);
+                  data = [...data, ...results.data];
+                  data = data.filter((d) => d.finished_at >= latestTime);
+                  request(
+                    {
+                      url: "https://dev.codementor.io/api/freelance-jobs",
+                      headers: {
+                        "x-codementor-api-key": "14f9ffcb7d4ed7efec12"
+                      }
+                    },
+                    (error, response, body) => {
+                      if (!error && response.statusCode === 200) {
+                        const results = JSON.parse(body);
+                        data = [...data, ...results.data];
+                        data = data.filter((d) => d.finished_at >= latestTime);
+                        data = data.sort(
+                          (a, b) => a.finished_at > b.finished_at
+                        );
+                        res.json(data);
+                      }
                     }
-                  },
-                  (error, response, body) => {
-                    if (!error && response.statusCode === 200) {
-                      const results = JSON.parse(body);
-                      data = [...data, ...results.data];
-                      res.json(data);
-                    }
-                  }
-                );
+                  );
+                }
               }
-            }
-          );
+            );
+          }
         }
+      );
+    } catch (error) {
+      console.log("failed: " + error.message);
+      res.end("failed: " + error.message);
+    }
+  })
+  .post((req, res, next) => {
+    latestTime = req.body.latestTime;
+    fs.writeFile("./latest", latestTime, (err) => {
+      if (err) {
+        console.error(err);
+        return;
       }
-    );
-  } catch (error) {
-    console.log("failed: " + error.message);
-    res.end("failed: " + error.message);
-  }
-});
+      res.end("success");
+    });
+  });
 
 module.exports = router;
